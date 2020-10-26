bdfda4484b95aacf85df067f8a65ec781ad6d8c3
w61644 <73499849+w61644@users.noreply.github.com>
2020-10-26 22:13:18 +0100

    Add files via upload

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("115e15d7-2d33-4544-8d92-a6c4fd6cc82d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PictureViewermasterpro;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            var file1Name = "img1.jpg";
+            var file2Name = "img2.jpg";
+
+            // act
+            var plik1 = Stat.SprawdzPlik(file1Name);
+            var plik2 = Stat.SprawdzPlik(file2Name);
+
+            // assert
+            Assert.IsTrue(plik1);
+            Assert.IsFalse(plik2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // arrange
+            var file1Name = "img1.jpg";
+            var file2Name = "UnitTest1.sh";
+
+            // act
+            var plik1 = Stat.SprawzCzyObraz(file1Name);
+            var plik2 = Stat.SprawzCzyObraz(file2Name);
+
+            // assert
+            Assert.IsTrue(plik1);
+            Assert.IsFalse(plik2);
+        }
+    }
+}
